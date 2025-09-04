e6d42bfe2fbb0e4fceac3bced3251454434cce4b
t.sekiguchi <maturibayasi1025@gmail.com>
2025-09-04 21:04:24 +0900

    ビルド後画面が表示されないエラーの修正

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,25 +1,5 @@
 // This file configures the initialization of Sentry on the client.
-// The added config here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+// Sentry has been disabled for Cloudflare Pages compatibility.
 
-import * as Sentry from "@sentry/nextjs";
-
-if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
-  Sentry.init({
-    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-    environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
-
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-
-    integrations: [Sentry.browserTracingIntegration()],
-    tracePropagationTargets: ["https://action.team-mir.ai"],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 0.1,
-  });
-}
-
-export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
+// 空のエクスポートでSentryを無効化
+export const onRouterTransitionStart = undefined;
--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -19,55 +18,5 @@ const nextConfig: NextConfig = {
   },
 };
 
-// Cloudflare Pages向けにSentryを無効化するオプション
-const shouldUseSentry = process.env.DISABLE_SENTRY !== "true";
-
-export default shouldUseSentry
-  ? withSentryConfig(nextConfig, {
-      // For all available options, see:
-      // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-      org: "team-mirai",
-      project: "action-board",
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-
-      // Suppress logs unless UPLOAD_SOURCEMAPS is set
-      silent: !process.env.UPLOAD_SOURCEMAPS,
-
-      // For all available options, see:
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-      // Upload a larger set of source maps for prettier stack traces (increases build time)
-      widenClientFileUpload: true,
-
-      // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-      // This can increase your server load as well as your hosting bill.
-      // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-      // side errors will fail.
-      tunnelRoute: "/monitoring",
-
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      disableLogger: true,
-
-      // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-      // See the following for more information:
-      // https://docs.sentry.io/product/crons/
-      // https://vercel.com/docs/cron-jobs
-      automaticVercelMonitors: false,
-
-      // Upload source maps to enable readable stack traces in errors
-      // See the following for more information:
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/sourcemaps/
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/build/#source-maps-options
-      sourcemaps: {
-        disable: !process.env.UPLOAD_SOURCEMAPS,
-        ignore: ["**/node_modules/**", "**/.next/cache/**", "**/tests/**"],
-        deleteSourcemapsAfterUpload: true,
-      },
-
-      release: {
-        name: process.env.SENTRY_RELEASE,
-        setCommits: undefined,
-      },
-    })
-  : nextConfig;
+// Sentry設定を完全に削除してCloudflare Pages互換性を確保
+export default nextConfig;
